b9c57d676def9ea8258d09e864ab765d799ce3c3
DIDAN HASAN MURTAQI <xmipa5.didanhasan@gmail.com>
2023-09-26 23:27:01 +0700

    Punya Didan

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,15 @@ function App() {
               college={"Universitas Diponegoro"}
               g
             />
+            <CardProfile
+              src={"https://i0.wp.com/dianisa.com/wp-content/uploads/2022/11/4-Gambar-pemandangan-gunung-dengan-sungai.jpg?resize=1280%2C720&ssl=1"}
+              name={"Didan Hasan Murtaqi"}
+              email={"didanxbass@gmail.com"}
+              title={"Mahasiswa"}
+              nim={"21120120140141"}
+              college={"Universitas Diponegoro"}
+              g
+            />
           </div>
           <div className="Form">
             <TextInput />
